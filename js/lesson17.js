ca8bf6afb083e49c8377d3ee27275a4086ff1c24
Bondarev-Igor <tosca@tut.by>
2021-02-07 23:17:43 +0300

    lesson18

---
+++ js/lesson17.js
@@ -0,0 +1,10 @@
+const str = "test";
+const arr = [1, 2, 3];
+console.log(arr.length);
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const logg = "Hello world";
+console.log(logg.slice(6  ));
