bae76f38e93a3418f66f084f652d7f5a3904e8a6
Bondarev-Igor <tosca@tut.by>
2021-02-18 23:19:45 +0300

    lesson23

---
+++ js/lesson23.js
@@ -0,0 +1,9 @@
+//Основы ООП. Прототипно-ориентированное наследование.
+
+let str = "some string";
+let strObj = new String(str);
+
+console.log(typeof (str));
+console.log(typeof(strObj));
+
+console.dir([1, 2, 3,]);
