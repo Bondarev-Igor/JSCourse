3bd53d98c001e0c35a76f67e1b7eff5d5c52c0db
Bondarev-Igor <tosca@tut.by>
2021-02-20 18:29:54 +0300

    ''

--- js/lesson23.js
+++ js/lesson23.js
@@ -7,3 +7,25 @@ console.log(typeof (str));
 console.log(typeof(strObj));
 
 console.dir([1, 2, 3,]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function (){
+        console.log('Hello');
+    }
+};
+
+const jonh = Object.create(soldier);
+
+const jonh = {
+    health: 100
+};
+
+// jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier)
+
+console.log(jonh.armor);
+console.log(jonh.health);
+jonh.sayHello();
