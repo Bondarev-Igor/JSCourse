c3fbbc3af01bcbbd9ac6fff8350c25c102f7143a
Bondarev-Igor <tosca@tut.by>
2021-02-28 03:12:32 +0300

    lesson26 comleted

---
+++ js/lesson26.js
@@ -0,0 +1,30 @@
+
+//to string
+//1.
+console.log(typeof(String(null)));
+
+//2.
+console.log(typeof(5 + ''));
+
+const num = 5;
+console.log('https://vk.com/' + num);
+
+const fonSize = 26 + 'px'; 
+
+//to number 
+//1.
+
+console.log(typeof(Number('4'))); 
+
+//2.
+console.log(typeof(+'5'));
+
+//3.
+console.log(typeof(parseInt('14px', 10)));
+console.log(parseInt('14px', 10));
+
+//to boolean
+//1.
+console.log(typeof(Boolean('4')));
+
+console.log(typeof(!!'333'));
